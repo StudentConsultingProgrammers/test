1f708c5974adccf8fef1a84a363511c4fd9e632d
erilov <erilov@S160502-HP001.studentconsulting.net>
2017-03-16 14:19:53 +0100

    first commit

---
+++ DatabasTest/src/testAvDBConnection/TestMySQLConnection.java
@@ -0,0 +1,34 @@
+package testAvDBConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestMySQLConnection {
+	public static String userName = "root";
+	public static String passWord = "root";
+	public static String dbUrl = 
+			"jdbc:mysql://localhost:3306/test2";
+	
+	
+	
+	
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		Connection cn = null;
+		try {
+			cn = DriverManager.getConnection(dbUrl, userName, passWord);
+			System.out.println("Established connection to db");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally{
+			if (cn!=null)
+				cn.close();
+		}
+		
+
+	}
+
+}
---
+++ DatabasTest/src/uiPackage/App.java
@@ -0,0 +1,13 @@
+package uiPackage;
+
+import databasePackage.DatabaseLayer;
+
+public class App {
+	
+	public static void main(String[] args) {
+		//App app = new App();
+		DatabaseLayer db = new DatabaseLayer();
+	}
+	
+
+}
